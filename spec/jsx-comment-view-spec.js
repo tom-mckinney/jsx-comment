fce8c0b8a227f69fa304b6c1ec300bd37c3db2c1
Tom McKinney <tmckinney@pileandcompany.com>
2016-12-29 13:05:31 -0500

    updated spec file + removed view and menu files

--- spec/jsx-comment-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import JsxCommentView from '../lib/jsx-comment-view';
-
-describe('JsxCommentView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
